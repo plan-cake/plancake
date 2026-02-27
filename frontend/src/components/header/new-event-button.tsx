6c25c6673edf2a98a7319dceb01c1757f7136f5f
jzgom067 <jzgombic3@gmail.com>
2026-02-26 21:16:56 -0500

    Change new event header button style
    By default it uses the secondary button, but it changes back to primary on dashboard and landing pages.

--- frontend/src/components/header/new-event-button.tsx
+++ frontend/src/components/header/new-event-button.tsx
@@ -4,6 +4,7 @@ import { PlusIcon } from "@radix-ui/react-icons";
 import { usePathname } from "next/navigation";
 
 import LinkButton from "@/features/button/components/link";
+import { ButtonStyle } from "@/features/button/props";
 
 export default function NewEventButton() {
   const pathname = usePathname();
@@ -12,9 +13,15 @@ export default function NewEventButton() {
     return null;
   }
 
+  let style: ButtonStyle = "secondary";
+
+  if (pathname === "/dashboard" || pathname === "/") {
+    style = "primary";
+  }
+
   return (
     <LinkButton
-      buttonStyle="primary"
+      buttonStyle={style}
       icon={<PlusIcon />}
       label="New Event"
       shrinkOnMobile
