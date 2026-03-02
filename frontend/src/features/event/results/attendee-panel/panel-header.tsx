b3e678d6ec870bf8fe71193828c9e6614de72017
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2026-03-02 12:46:08 -0500

    Revert "rename mobileResults to desktopResults"
    This reverts commit f083da9d360877a2103682e452d0d8162faef302.

--- frontend/src/features/event/results/attendee-panel/panel-header.tsx
+++ frontend/src/features/event/results/attendee-panel/panel-header.tsx
@@ -45,11 +45,11 @@ export default function PanelHeader({
   return (
     <div
       className={cn(
-        "flex w-full touch-none select-none justify-between",
+        "flex touch-none select-none justify-between",
         inDrawer ? "" : "px-6 pt-6",
       )}
     >
-      <div className="flex flex-col items-start">
+      <div className="flex flex-col">
         <h2 className="text-md font-semibold">
           {isRemoving ? "Removing a" : "A"}ttendees
         </h2>
