35b38c57aa246ff535d3fffabc75e4007401dd09
Daniel Shi <144500568+danielshid@users.noreply.github.com>
2026-03-02 05:17:04 +0000

    added focus ring to dropdowns

--- frontend/src/features/selector/components/dropdown.tsx
+++ frontend/src/features/selector/components/dropdown.tsx
@@ -26,8 +26,10 @@ export default function Dropdown<TValue extends string | number>({
       <Select.Trigger
         id={id}
         className={cn(
-          "text-accent inline-flex items-center rounded-2xl text-start hover:cursor-pointer focus:outline-none",
+          "text-accent inline-flex items-center rounded-2xl text-start hover:cursor-pointer",
           "bg-accent/15 hover:bg-accent/25 active:bg-accent/40 px-3 py-1",
+          "focus-visible:rounded-full focus-visible:outline-2",
+          "focus-visible:outline-foreground focus-visible:outline-offset-2",
           disabled &&
             "bg-foreground/20 text-foreground hover:bg-foreground/20 active:bg-foreground/20 cursor-not-allowed opacity-50 hover:cursor-not-allowed",
           className,
