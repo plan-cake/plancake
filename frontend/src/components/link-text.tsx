0cd10147fc2156c9e2f768f2f69c0b3d093aa1b9
jzgom067 <jzgombic3@gmail.com>
2026-03-02 20:40:59 -0500

    Change LinkText to use font-bold

--- frontend/src/components/link-text.tsx
+++ frontend/src/components/link-text.tsx
@@ -2,7 +2,11 @@ import { ReactNode } from "react";
 
 export default function LinkText({ children }: { children: ReactNode }) {
   return (
-    <span className={"hover:text-accent cursor-pointer hover:underline"}>
+    <span
+      className={
+        "hover:text-accent cursor-pointer font-extrabold hover:underline"
+      }
+    >
       {children}
     </span>
   );
