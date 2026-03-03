bb689bf513fc87c1b8f53dee45643ec9e71b4bcc
jzgom067 <jzgombic3@gmail.com>
2026-03-02 20:44:05 -0500

    Add unbolded option to LinkText

--- frontend/src/components/link-text.tsx
+++ frontend/src/components/link-text.tsx
@@ -1,11 +1,20 @@
 import { ReactNode } from "react";
 
-export default function LinkText({ children }: { children: ReactNode }) {
+import { cn } from "@/lib/utils/classname";
+
+export default function LinkText({
+  unbolded = false,
+  children,
+}: {
+  unbolded?: boolean;
+  children: ReactNode;
+}) {
   return (
     <span
-      className={
-        "hover:text-accent cursor-pointer font-extrabold hover:underline"
-      }
+      className={cn(
+        "hover:text-accent cursor-pointer hover:underline",
+        unbolded ? "font-normal" : "font-extrabold",
+      )}
     >
       {children}
     </span>
