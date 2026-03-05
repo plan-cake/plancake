c376ccbe6beacb56ecf1d8f234fd2c62d18ad75b
Daniel Shi <144500568+danielshid@users.noreply.github.com>
2026-03-05 20:29:08 +0000

    added focus ring styling to dropdown options

--- frontend/src/features/selector/components/dropdown.tsx
+++ frontend/src/features/selector/components/dropdown.tsx
@@ -72,7 +72,12 @@ const DropdownItem = forwardRef<HTMLDivElement, DropdownItemProps>(
       <Select.Item
         ref={ref}
         value={value.toString()}
-        className="data-[highlighted]:bg-accent relative flex h-[30px] select-none items-center rounded-xl px-6 leading-none hover:outline-none data-[disabled]:pointer-events-none data-[disabled]:text-gray-400 data-[highlighted]:text-white"
+        className={cn(
+          "data-[highlighted]:bg-accent relative flex h-[30px] select-none items-center rounded-xl px-6 leading-none",
+          "hover:outline-none data-[disabled]:pointer-events-none data-[disabled]:text-gray-400 data-[highlighted]:text-white",
+          "data-[highlighted]:outline-foreground data-[highlighted]:outline data-[highlighted]:outline-2 data-[highlighted]:outline-offset-2",
+          "focus-visible:rounded-xl focus-visible:outline-2 focus-visible:outline-foreground focus-visible:outline-offset-2",
+        )}
       >
         <Select.ItemText>{children}</Select.ItemText>
         <Select.ItemIndicator className="absolute left-2 inline-flex w-4 items-center justify-center">
