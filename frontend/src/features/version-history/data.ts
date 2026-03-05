6f2db48c712ff84930f5173588f82ead24bbb1a7
jzgom067 <jzgombic3@gmail.com>
2026-03-05 14:03:56 -0500

    Add v0.3.2 version data

--- frontend/src/features/version-history/data.ts
+++ frontend/src/features/version-history/data.ts
@@ -89,6 +89,21 @@ export function getVersionHistoryData(): VersionHistoryData {
             "Increased toast lifetime for easier reading",
             "Fixed an issue with layering order on the painting page",
           ],
+        },
+        {
+          version: "v0.3.2",
+          releaseDate: { year: 2026, month: 2, day: 5 },
+          changes: [
+            "Added a reminder to view all grid pages when filling out availability",
+            "Updated dashboard to dynamically show more participants on each event",
+            "Updated the version history page layout",
+            "Updated the new event button to have dynamic styling",
+            "Updated drawers across the site to be more interactive",
+            "Updated selector drawer appearance to show more options at once",
+            "Updated the layout of authentication-related pages",
+            "Fixed an issue where nicknames would attempt to autofill on initial save",
+            "Fixed an issue where dialogs would look blurry on certain browsers",
+          ],
         }
       ],
     },
