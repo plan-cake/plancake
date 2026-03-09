b726e1d321ee8f4f627e57847f31f49ed11ad674
jzgom067 <jzgombic3@gmail.com>
2026-03-09 19:59:41 -0400

    Fix offset logo text

--- frontend/src/components/logo.tsx
+++ frontend/src/components/logo.tsx
@@ -2,13 +2,13 @@ import { cn } from "@/lib/utils/classname";
 
 export default function Logo({ oneLine = false }: { oneLine?: boolean }) {
   return (
-    <div className="font-display text-lion text-2xl font-normal [-webkit-text-stroke:1px_black]">
+    <div className="font-display text-lion select-none text-2xl font-normal [-webkit-text-stroke:1px_black]">
       <div>
         <span>plan</span>
         <span
           className={cn(
             "duration-250 transition-opacity ease-out",
-            oneLine ? "opacity-100" : "opacity-0",
+            oneLine ? "opacity-100" : "absolute opacity-0",
           )}
         >
           cake
