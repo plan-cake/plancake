bbfff04458a1e484b74e3b9706b684aa60d5e70c
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2026-03-13 09:26:42 -0400

    disable only show best times

--- frontend/src/features/event/results/view-settings.tsx
+++ frontend/src/features/event/results/view-settings.tsx
@@ -1,6 +1,6 @@
-import Checkbox from "@/components/checkbox";
+// import Checkbox from "@/components/checkbox";
 import TimeZoneSelector from "@/features/event/components/selectors/timezone";
-import { useResultsContext } from "@/features/event/results/context";
+// import { useResultsContext } from "@/features/event/results/context";
 
 export default function ViewSettings({
   timezone,
@@ -15,28 +15,28 @@ export default function ViewSettings({
   setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
   drawerNesting?: number;
 }) {
-  const { showOnlyBestTimes, setShowOnlyBestTimes } = useResultsContext();
+  // const { showOnlyBestTimes, setShowOnlyBestTimes } = useResultsContext();
 
   return (
     <>
-      <Checkbox
+      {/* <Checkbox
         label="Only show best times"
         checked={showOnlyBestTimes}
         onChange={setShowOnlyBestTimes}
-      />
-      <div className="mt-3">
-        Displaying event in
-        <span className="text-accent font-bold">
-          <TimeZoneSelector
-            id="timezone-select"
-            value={timezone}
-            onChange={onTimezoneChange}
-            drawerNesting={drawerNesting}
-            open={open}
-            onOpenChange={setOpen}
-          />
-        </span>
-      </div>
+      /> */}
+      {/* <div className="mt-3"> */}
+      Displaying event in
+      <span className="text-accent font-bold">
+        <TimeZoneSelector
+          id="timezone-select"
+          value={timezone}
+          onChange={onTimezoneChange}
+          drawerNesting={drawerNesting}
+          open={open}
+          onOpenChange={setOpen}
+        />
+      </span>
+      {/* </div> */}
     </>
   );
 }
