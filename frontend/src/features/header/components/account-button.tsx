eafabd4ff1751956a063191f2dff66f53f8d60d2
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2026-03-30 13:23:40 -0400

    fix merge conflicts

--- frontend/src/features/header/components/account-button.tsx
+++ frontend/src/features/header/components/account-button.tsx
@@ -1,16 +1,14 @@
-"use client";
-
 import { ExitIcon, PersonIcon } from "@radix-ui/react-icons";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 import KebabMenu from "@/components/kebab-menu";
-import { useAccount } from "@/features/account/context";
+import { getSession } from "@/features/account/get-session";
 import ActionButton from "@/features/button/components/action";
 import EmptyButton from "@/features/button/components/empty";
 import LinkButton from "@/features/button/components/link";
 import ShrinkingHeaderButton from "@/features/header/components/shrinking-header-button";
-import { useToast } from "@/features/system-feedback";
-import { MESSAGES } from "@/lib/messages";
+// import { useToast } from "@/features/system-feedback";
+// import { MESSAGES } from "@/lib/messages";
 import { clientPost } from "@/lib/utils/api/client-fetch";
 import { ROUTES } from "@/lib/utils/api/endpoints";
 import { ApiErrorResponse } from "@/lib/utils/api/fetch-wrapper";
@@ -19,23 +17,22 @@ interface AccountButtonProps {
   onMenuOpenChange?: (isOpen: boolean) => void;
 }
 
-export default function AccountButton({
+export default async function AccountButton({
   onMenuOpenChange,
 }: AccountButtonProps) {
-  const { loginState, logout, accountDetails } = useAccount();
-  const router = useRouter();
-  const { addToast } = useToast();
+  const accountDetails = await getSession();
+
+  // const { addToast } = useToast();
 
   const signOut = async () => {
     try {
       await clientPost(ROUTES.auth.logout);
-      logout();
-      router.push("/login");
-      addToast("success", MESSAGES.SUCCESS_LOGOUT);
-      return true;
+      redirect("/login");
+      // cannot use toast after redirect bc toast does not work on server components
     } catch (e) {
       const error = e as ApiErrorResponse;
-      addToast("error", error.formattedMessage);
+      console.error("Logout error:", error);
+      // addToast("error", error.formattedMessage);
       return false;
     }
   };
@@ -58,7 +55,7 @@ export default function AccountButton({
     />
   );
 
-  if (loginState === "logged_in") {
+  if (accountDetails) {
     return (
       <ShrinkingHeaderButton
         buttonStyle="frosted glass inset"
