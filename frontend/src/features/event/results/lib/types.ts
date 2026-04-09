402f703af2d7b1986be551ce6017140d2ecc8368
jzgom067 <jzgombic3@gmail.com>
2026-04-09 14:43:26 -0400

    Change eventType to use EventType type

--- frontend/src/features/event/results/lib/types.ts
+++ frontend/src/features/event/results/lib/types.ts
@@ -1,8 +1,9 @@
+import { EventType } from "@/core/event/types";
 import { AllAvailability } from "@/lib/utils/api/types";
 
 export type ResultsInformation = {
   eventCode: string;
-  eventType: string;
+  eventType: EventType;
   isCreator: boolean;
   participants: AllAvailability["participants"];
   availability: AllAvailability["availability"];
