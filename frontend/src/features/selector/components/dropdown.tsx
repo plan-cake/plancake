6cf0a4f178b9598e264ac68b1c43d115f8852e1e
jzgom067 <jzgombic3@gmail.com>
2026-04-08 20:50:08 -0400

    Fix dropdown focus outlines

--- frontend/src/features/selector/components/dropdown.tsx
+++ frontend/src/features/selector/components/dropdown.tsx
@@ -26,7 +26,7 @@ export default function Dropdown<TValue extends string | number>({
       <Select.Trigger
         id={id}
         className={cn(
-          "text-accent-text inline-flex items-center rounded-2xl text-start hover:cursor-pointer focus:outline-none",
+          "text-accent-text inline-flex items-center rounded-2xl text-start hover:cursor-pointer",
           "bg-accent/15 hover:bg-accent/25 active:bg-accent/40 px-3 py-1",
           "focus-visible:rounded-full focus-visible:outline-2",
           "focus-visible:outline-foreground focus-visible:outline-offset-2",
