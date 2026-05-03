e406168127d6aff6651751e1f4d29c870dcc24a8
jzgom067 <jzgombic3@gmail.com>
2026-05-03 19:18:18 -0400

    Set closeOnClick on theme picker

--- frontend/src/features/header/components/theme-toggle.tsx
+++ frontend/src/features/header/components/theme-toggle.tsx
@@ -30,6 +30,7 @@ export default function ThemeToggle() {
             icon={<SunMoonIcon />}
           />
         }
+        closeOnClick={false}
       >
         <SegmentedControl
           options={[
