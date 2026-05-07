96cf37ba7a7110edd1aa934a816f288de230429b
jzgom067 <jzgombic3@gmail.com>
2026-05-07 18:37:59 -0400

    Simplify specific date range component

--- frontend/src/features/event/editor/date-range/specific-date-display.tsx
+++ frontend/src/features/event/editor/date-range/specific-date-display.tsx
@@ -13,37 +13,52 @@ export default function SpecificDateRangeDisplay({
   endDate,
   open = false,
 }: SpecificDateRangeDisplayProps) {
-  const displayFrom = startDate ? format(startDate, "EEE MMMM d, yyyy") : "";
-  const displayTo = endDate ? format(endDate, "EEE MMMM d, yyyy") : "";
+  return (
+    <form className="flex w-full flex-col gap-y-2 md:flex-row md:gap-4">
+      <SpecificDateDisplay
+        mobileLabel="FROM"
+        date={startDate}
+        ariaLabel="Start date"
+        open={open}
+      />
+
+      <span className="hidden w-fit py-1 text-gray-400 md:block">TO</span>
+
+      <SpecificDateDisplay
+        mobileLabel="UNTIL"
+        date={endDate}
+        ariaLabel="End date"
+        open={open}
+      />
+    </form>
+  );
+}
+
+function SpecificDateDisplay({
+  mobileLabel,
+  date,
+  ariaLabel,
+  open = false,
+}: {
+  mobileLabel: string;
+  date: Date;
+  ariaLabel: string;
+  open?: boolean;
+}) {
+  const displayDate = format(date, "EEE MMMM d, yyyy");
 
   const displayStyle =
     "text-accent-text bg-accent/15 hover:bg-accent/25 active:bg-accent/40 rounded-2xl px-3 py-1 focus:outline-none";
 
   return (
-    <form className="flex w-full flex-col gap-y-2 md:flex-row md:gap-4">
-      {/* Start Date */}
-      <div className="flex w-fit items-center space-x-4">
-        <p className="text-gray-400 md:hidden">FROM</p>
-        <span
-          className={cn(displayStyle, open && "ring-accent ring-1")}
-          aria-label="Start date"
-        >
-          {displayFrom}
-        </span>
-      </div>
-
-      <span className="hidden w-fit py-1 text-gray-400 md:block">TO</span>
-
-      {/* End Date */}
-      <div className="flex w-fit items-center space-x-4">
-        <p className="text-gray-400 md:hidden">UNTIL</p>
-        <span
-          className={cn(displayStyle, open && "ring-accent ring-1")}
-          aria-label="End date"
-        >
-          {displayTo}
-        </span>
-      </div>
-    </form>
+    <div className="flex w-fit items-center space-x-4">
+      <p className="text-gray-400 md:hidden">{mobileLabel}</p>
+      <span
+        className={cn(displayStyle, open && "ring-accent ring-1")}
+        aria-label={ariaLabel}
+      >
+        {displayDate}
+      </span>
+    </div>
   );
 }
