1f1effb852ec4cb91fba65670d848e7ee01c211c
jzgom067 <jzgombic3@gmail.com>
2026-05-08 16:46:28 -0400

    Remove dashed outline state from weekday row

--- frontend/src/features/event/editor/date-range/calendars/weekday.tsx
+++ frontend/src/features/event/editor/date-range/calendars/weekday.tsx
@@ -36,15 +36,12 @@ export default function WeekdayCalendar({
     .map((d) => ALL_WEEKDAYS.indexOf(d))
     .filter((i) => i !== -1);
 
-  let highlightState = "inactive";
   let highlightStart = -1;
   let highlightEnd = -1;
   if (selectedIndices.length > 0 && anchorIndex === null) {
-    highlightState = "active";
     highlightStart = Math.min(...selectedIndices);
     highlightEnd = Math.max(...selectedIndices);
   } else if (anchorIndex !== null) {
-    highlightState = "hover";
     highlightStart = Math.min(anchorIndex, hoverIndex ?? anchorIndex);
     highlightEnd = Math.max(anchorIndex, hoverIndex ?? anchorIndex);
   }
@@ -52,10 +49,7 @@ export default function WeekdayCalendar({
   return (
     <div className="flex w-full select-none flex-row flex-wrap">
       {ALL_WEEKDAYS.map((day, index) => {
-        const isHighlighted =
-          index >= highlightStart &&
-          index <= highlightEnd &&
-          highlightState !== "inactive";
+        const isHighlighted = index >= highlightStart && index <= highlightEnd;
         const isRangeStart = index === highlightStart;
         const isRangeEnd = index === highlightEnd;
 
@@ -74,19 +68,9 @@ export default function WeekdayCalendar({
                 // Inactive State
                 !isHighlighted && "hover:bg-accent/15 rounded-full",
 
-                // Active State
+                // Highlighted State
                 isHighlighted &&
-                  highlightState === "active" &&
                   "bg-accent/15 text-accent-text hover:bg-accent/30",
-
-                // Hovered state
-                isHighlighted &&
-                  highlightState === "hover" &&
-                  cn(
-                    "border-foreground/75 border-b-2 border-t-2 border-dashed",
-                    isRangeStart ? "pl-5.5 border-l-2" : "",
-                    isRangeEnd ? "pr-5.5 border-r-2" : "",
-                  ),
 
                 // Contiguous Rounding Logic
                 isRangeStart && "rounded-l-full",
