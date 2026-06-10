fddddd422aeda6a8348c3c07723a1ef4c54c14e7
jzgom067 <jzgombic3@gmail.com>
2026-06-10 16:00:48 -0400

    Simplify share button variable logic

--- frontend/src/features/event/results/share-menu.tsx
+++ frontend/src/features/event/results/share-menu.tsx
@@ -18,38 +18,27 @@ export default function ShareMenu({
 }) {
   const { addToast } = useToast();
 
-  let shareButton: React.ReactNode = (
-    <ActionButton
-      buttonStyle="primary"
-      icon={<ShareIcon />}
-      label="Share"
-      onClick={async () => {
-        try {
-          await navigator.share({
-            title: eventTitle,
-            url: window.location.href,
-          });
-        } catch (error) {
-          // An error is thrown if sharing is cancelled, ignore that
-          if (error instanceof Error && error.name !== "AbortError") {
-            addToast("error", MESSAGES.ERROR_GENERIC);
+  const shareButton: React.ReactNode =
+    typeof navigator !== "undefined" && navigator.share ? (
+      <ActionButton
+        buttonStyle="primary"
+        icon={<ShareIcon />}
+        label="Share"
+        onClick={async () => {
+          try {
+            await navigator.share({
+              title: eventTitle,
+              url: window.location.href,
+            });
+          } catch (error) {
+            // An error is thrown if sharing is cancelled, ignore that
+            if (error instanceof Error && error.name !== "AbortError") {
+              addToast("error", MESSAGES.ERROR_GENERIC);
+            }
           }
-        }
-      }}
-    />
-  );
-  // Check if sharing is supported
-  if (typeof navigator !== "undefined" && !navigator.share) {
-    /* This condition means it will be rendered until mounted on the client, then it
-     * disappears if not supported. There are more browsers that support the API than
-     * don't, so this is a better trade-off than having the button appear after initial
-     * mount on supported browsers.
-     *
-     * This also won't be visible on mobile anyway, since the buttons are hidden in the
-     * kebab menu.
-     */
-    shareButton = null;
-  }
+        }}
+      />
+    ) : null;
 
   const copyButton = (
     <CopyToastButton code={eventCode} buttonStyle="secondary" />
