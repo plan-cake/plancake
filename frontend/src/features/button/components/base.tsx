424f510b791a69089bc492603b1401473dea4745
jzgom067 <jzgombic3@gmail.com>
2026-06-11 16:16:24 -0400

    Add tooltip prop to buttons

--- frontend/src/features/button/components/base.tsx
+++ frontend/src/features/button/components/base.tsx
@@ -3,15 +3,16 @@
 import {
   ReactElement,
   cloneElement,
+  forwardRef,
   useEffect,
   useState,
-  forwardRef,
 } from "react";
 
 import Link from "next/link";
 
 import LoadingSpinner from "@/components/loading-spinner";
 import { BaseButtonProps, ButtonStyle } from "@/features/button/props";
+import Tooltip from "@/features/system-feedback/tooltip/base";
 import { cn } from "@/lib/utils/classname";
 
 type ButtonState = "rest" | "loading" | "disabled";
@@ -28,6 +29,7 @@ const BaseButton = forwardRef<Ref, BaseButtonProps>(
       icon,
       label,
       shrinkOnMobile = false,
+      tooltip,
       loading = false,
       disabled = false,
       href,
@@ -122,8 +124,10 @@ const BaseButton = forwardRef<Ref, BaseButtonProps>(
       </div>
     );
 
+    let buttonComponent;
+
     if (disabled || isLoading) {
-      return (
+      buttonComponent = (
         <button
           disabled
           type={type}
@@ -134,7 +138,7 @@ const BaseButton = forwardRef<Ref, BaseButtonProps>(
         </button>
       );
     } else if (_buttontype === "link") {
-      return (
+      buttonComponent = (
         <Link
           ref={ref as React.Ref<HTMLAnchorElement>}
           {...props}
@@ -145,7 +149,7 @@ const BaseButton = forwardRef<Ref, BaseButtonProps>(
         </Link>
       );
     } else {
-      return (
+      buttonComponent = (
         <button
           type={type}
           ref={ref as React.Ref<HTMLButtonElement>}
@@ -157,6 +161,12 @@ const BaseButton = forwardRef<Ref, BaseButtonProps>(
         </button>
       );
     }
+
+    if (typeof tooltip !== "undefined") {
+      buttonComponent = <Tooltip content={tooltip}>{buttonComponent}</Tooltip>;
+    }
+
+    return buttonComponent;
   },
 );
 
