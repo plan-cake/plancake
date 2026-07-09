e5b781ebe73f012f01e0e45f8a8f920d9a16e8a8
jzgom067 <jzgombic3@gmail.com>
2026-07-09 17:45:57 -0400

    Update guest mode banner text

--- frontend/src/app/dashboard/page-client.tsx
+++ frontend/src/app/dashboard/page-client.tsx
@@ -83,15 +83,15 @@ export default function ClientPage({
       {!logged_in && (
         <Banner type="info" title="Currently in Guest Mode">
           <div>
-            This data is only available from this browser.{" "}
+            This data is only available from this browser,{" "}
             <Link
               href="/register"
               className="text-accent cursor-pointer font-bold hover:underline"
             >
-              Create an account
+              create an account
             </Link>{" "}
-            to sync data across devices. You can import this data at any time in
-            account settings.
+            to sync across devices. You can import this data into your account
+            at any time in account settings.
           </div>
         </Banner>
       )}
