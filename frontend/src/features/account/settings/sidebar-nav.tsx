ee47898e13ad5c05b31071653a198a0408ee2a2e
jzgom067 <jzgombic3@gmail.com>
2026-07-09 21:34:44 -0400

    Add aria-current to settings nav

--- frontend/src/features/account/settings/sidebar-nav.tsx
+++ frontend/src/features/account/settings/sidebar-nav.tsx
@@ -46,6 +46,7 @@ export default function SettingsNav() {
             <Link
               key={tab.href}
               href={tab.href}
+              aria-current={isActive ? "page" : undefined}
               className={cn(
                 "text-md focus-visible:ring-primary/50 relative flex items-center whitespace-nowrap rounded-full px-4 py-2.5 font-medium outline-none focus-visible:ring-2",
                 isActive
