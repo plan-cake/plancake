cb929e9aacdc865ccc090bc8ff446159cb690a91
jzgom067 <jzgombic3@gmail.com>
2026-07-21 21:10:22 -0400

    Create useViewTransition hook

---
+++ frontend/src/lib/hooks/use-view-transition.ts
@@ -0,0 +1,37 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export function useViewTransition() {
+  const router = useRouter();
+
+  const doViewTransition = (href: string, timeout: number = 2000) => {
+    if (!document.startViewTransition) {
+      router.push(href);
+      return;
+    }
+
+    document.startViewTransition(() => {
+      return new Promise<void>((resolve) => {
+        const observer = new MutationObserver(() => {
+          observer.disconnect();
+          resolve();
+        });
+
+        observer.observe(document.body, {
+          childList: true,
+          subtree: true,
+        });
+
+        router.push(href);
+
+        setTimeout(() => {
+          observer.disconnect();
+          resolve();
+        }, timeout);
+      });
+    });
+  };
+
+  return doViewTransition;
+}
