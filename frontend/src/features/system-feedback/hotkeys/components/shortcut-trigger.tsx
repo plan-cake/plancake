3cdf040114380b6250c505931f146a0c97b3afee
jzgom067 <jzgombic3@gmail.com>
2026-08-08 16:48:34 -0400

    Add textfield support to shortcut trigger

--- frontend/src/features/system-feedback/hotkeys/components/shortcut-trigger.tsx
+++ frontend/src/features/system-feedback/hotkeys/components/shortcut-trigger.tsx
@@ -40,6 +40,7 @@ export default function ShortcutTrigger({
       }
       if (targetEl) {
         targetEl.click();
+        targetEl.focus();
       }
     }
     endShortcutMode();
@@ -47,6 +48,7 @@ export default function ShortcutTrigger({
 
   useHotkeys(hotkey, handleShortcut, {
     scopes: [SHORTCUT_MODE_SCOPE],
+    preventDefault: true,
   });
 
   return (
