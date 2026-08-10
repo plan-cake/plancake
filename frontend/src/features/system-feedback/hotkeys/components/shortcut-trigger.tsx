19d6aa819a7f80196abfcc78a0da4475679295ce
jzgom067 <jzgombic3@gmail.com>
2026-08-09 21:37:46 -0400

    Add forwardRef to ShortcutTrigger

--- frontend/src/features/system-feedback/hotkeys/components/shortcut-trigger.tsx
+++ frontend/src/features/system-feedback/hotkeys/components/shortcut-trigger.tsx
@@ -1,5 +1,6 @@
-import { useRef } from "react";
+import { forwardRef, HTMLAttributes, useRef } from "react";
 
+import { Slot } from "@radix-ui/react-slot";
 import { useHotkeys } from "react-hotkeys-hook";
 
 import ShortcutTooltip from "@/features/system-feedback/hotkeys/components/shortcut-tooltip";
@@ -8,16 +9,7 @@ import { useShortcuts } from "@/features/system-feedback/hotkeys/context";
 import { TooltipSide } from "@/features/system-feedback/tooltip/base";
 import { cn } from "@/lib/utils/classname";
 
-export default function ShortcutTrigger({
-  hotkey,
-  selector,
-  onAction,
-  tooltipSide,
-  allowTooltipCollisions = false,
-  disabled = false,
-  children,
-  className,
-}: {
+type ShortcutTriggerProps = HTMLAttributes<HTMLElement> & {
   hotkey: string;
   selector?: string;
   onAction?: () => void;
@@ -25,46 +17,68 @@ export default function ShortcutTrigger({
   allowTooltipCollisions?: boolean;
   disabled?: boolean;
   children: React.ReactNode;
-  className?: string;
-}) {
-  const { endShortcutMode } = useShortcuts();
-  const wrapperRef = useRef<HTMLDivElement>(null);
+};
 
-  const handleShortcut = () => {
-    if (onAction) {
-      onAction();
-    } else {
-      let targetEl: HTMLElement | null = null;
-      if (selector) {
-        targetEl = wrapperRef.current?.querySelector(selector) as HTMLElement;
+const ShortcutTrigger = forwardRef<HTMLElement, ShortcutTriggerProps>(
+  (
+    {
+      hotkey,
+      selector,
+      onAction,
+      tooltipSide,
+      allowTooltipCollisions = false,
+      disabled = false,
+      children,
+      className,
+      ...slotProps
+    },
+    forwardedRef,
+  ) => {
+    const { endShortcutMode } = useShortcuts();
+    const wrapperRef = useRef<HTMLDivElement>(null);
+
+    const handleShortcut = () => {
+      if (onAction) {
+        onAction();
       } else {
-        targetEl = wrapperRef.current?.firstElementChild as HTMLElement;
+        let targetEl: HTMLElement | null = null;
+        if (selector) {
+          targetEl = wrapperRef.current?.querySelector(selector) as HTMLElement;
+        } else {
+          targetEl = wrapperRef.current?.firstElementChild as HTMLElement;
+        }
+        if (targetEl) {
+          targetEl.click();
+          targetEl.focus();
+        }
       }
-      if (targetEl) {
-        targetEl.click();
-        targetEl.focus();
-      }
-    }
-    endShortcutMode(false);
-  };
+      endShortcutMode(false);
+    };
 
-  useHotkeys(hotkey, handleShortcut, {
-    scopes: [SHORTCUT_MODE_SCOPE],
-    preventDefault: true,
-    enabled: !disabled,
-  });
+    useHotkeys(hotkey, handleShortcut, {
+      scopes: [SHORTCUT_MODE_SCOPE],
+      preventDefault: true,
+      enabled: !disabled,
+    });
 
-  return (
-    <div ref={wrapperRef} className={cn(className, "relative")}>
-      {children}
-      <ShortcutTooltip
-        hotkey={hotkey}
-        side={tooltipSide}
-        allowCollisions={allowTooltipCollisions}
-        disabled={disabled}
-      >
-        <div className="pointer-events-none absolute inset-0" />
-      </ShortcutTooltip>
-    </div>
-  );
-}
+    return (
+      <div ref={wrapperRef} className={cn(className, "relative")}>
+        <Slot ref={forwardedRef} {...slotProps}>
+          {children}
+        </Slot>
+        <ShortcutTooltip
+          hotkey={hotkey}
+          side={tooltipSide}
+          allowCollisions={allowTooltipCollisions}
+          disabled={disabled}
+        >
+          <div className="pointer-events-none absolute inset-0" />
+        </ShortcutTooltip>
+      </div>
+    );
+  },
+);
+
+ShortcutTrigger.displayName = "ShortcutTrigger";
+
+export default ShortcutTrigger;
