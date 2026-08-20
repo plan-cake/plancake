4c0f8e5717bf836f1083d6e3a2f1016b97e87a71
jzgom067 <jzgombic3@gmail.com>
2026-08-20 13:35:23 -0400

    Fix merge issue

--- frontend/src/features/account/setting-dialogs/change-password/main-dialog.tsx
+++ frontend/src/features/account/setting-dialogs/change-password/main-dialog.tsx
@@ -8,8 +8,10 @@ import ResetStep from "@/features/account/setting-dialogs/change-password/steps/
 import { useChangePasswordFlow } from "@/features/account/setting-dialogs/change-password/use-change-password";
 import EmptyButton from "@/features/button/components/empty";
 import { FormDialog } from "@/features/system-feedback";
+import useCheckMobile from "@/lib/hooks/use-check-mobile";
 
 export default function ChangePasswordDialog() {
+  const isMobile = useCheckMobile();
   const flow = useChangePasswordFlow();
 
   const [renderedStep, setRenderedStep] = useState(flow.step);
