edf01a141a3421723fe0c02e497edf7ad748ec79
jzgom067 <jzgombic3@gmail.com>
2026-08-29 20:27:47 -0400

    Create centralized date popover

--- frontend/src/features/event/editor/date-range/popover.tsx
+++ frontend/src/features/event/editor/date-range/popover.tsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
 
-import * as Popover from "@radix-ui/react-popover";
-
 import { useEventContext } from "@/core/event/context";
+import DatePopover from "@/features/event/editor/date-popover";
 import { Calendar } from "@/features/event/editor/date-range/calendar";
 import { SpecificDateRangeDisplayProps } from "@/features/event/editor/date-range/date-range-props";
 import DateRangePresets from "@/features/event/editor/date-range/presets";
 import SpecificDateRangeDisplay from "@/features/event/editor/date-range/specific-date-display";
-import { cn } from "@/lib/utils/classname";
 
 export default function DateRangePopover({
   earliestDate,
@@ -17,33 +15,20 @@ export default function DateRangePopover({
   const [open, setOpen] = useState(false);
 
   return (
-    <Popover.Root open={open} onOpenChange={setOpen}>
-      <Popover.Trigger className="hover:cursor-pointer">
-        <SpecificDateRangeDisplay dates={dates} open={open} />
-      </Popover.Trigger>
-
-      <Popover.Portal>
-        <Popover.Content
-          align="start"
-          sideOffset={10}
-          className={cn(
-            "bg-background z-50 rounded-2xl border border-gray-400 p-4 shadow-lg",
-            "flex flex-col items-center gap-2",
-            "data-[state=open]:animate-slideUpAndFade",
-            "data-[state=closed]:animate-slideDownAndFadeOut",
-          )}
-          aria-label="Date range picker"
-        >
-          <Calendar
-            earliestDate={earliestDate}
-            className="w-fit"
-            selectedDates={dates}
-            setDates={setDates}
-            dateRangeError={errors.dateRange}
-          />
-          <DateRangePresets dates={dates} setDates={setDates} />
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
+    <DatePopover
+      open={open}
+      setOpen={setOpen}
+      trigger={<SpecificDateRangeDisplay dates={dates} open={open} />}
+      ariaLabel="Date range picker"
+    >
+      <Calendar
+        earliestDate={earliestDate}
+        className="w-fit"
+        selectedDates={dates}
+        setDates={setDates}
+        dateRangeError={errors.dateRange}
+      />
+      <DateRangePresets dates={dates} setDates={setDates} />
+    </DatePopover>
   );
 }
--- frontend/src/features/event/editor/weekday-range/popover.tsx
+++ frontend/src/features/event/editor/weekday-range/popover.tsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
 
-import * as Popover from "@radix-ui/react-popover";
-
 import { useEventContext } from "@/core/event/context";
 import { Weekday } from "@/core/event/types";
+import DatePopover from "@/features/event/editor/date-popover";
 import Calendar from "@/features/event/editor/weekday-range/calendar";
 import WeekdayRangePresets from "@/features/event/editor/weekday-range/presets";
 import WeekdayRangeDisplay from "@/features/event/editor/weekday-range/weekday-range-display";
-import { cn } from "@/lib/utils/classname";
 
 export default function WeekdayRangePopover({
   weekdays,
@@ -18,31 +16,19 @@ export default function WeekdayRangePopover({
   const [open, setOpen] = useState(false);
 
   return (
-    <Popover.Root open={open} onOpenChange={setOpen}>
-      <Popover.Trigger className="hover:cursor-pointer">
-        <WeekdayRangeDisplay weekdays={weekdays} open={open} />
-      </Popover.Trigger>
-
-      <Popover.Portal>
-        <Popover.Content
-          align="start"
-          sideOffset={10}
-          className={cn(
-            "bg-background z-50 rounded-2xl border border-gray-400 p-4 shadow-lg",
-            "data-[state=open]:animate-slideUpAndFade",
-            "data-[state=closed]:animate-slideDownAndFadeOut",
-          )}
-          aria-label="Weekday range picker"
-        >
-          <div className="flex flex-col gap-2">
-            <Calendar selectedDays={weekdays} onChange={setWeekdayRange} />
-            <WeekdayRangePresets
-              weekdays={weekdays}
-              setWeekdayRange={setWeekdayRange}
-            />
-          </div>
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
+    <DatePopover
+      trigger={<WeekdayRangeDisplay weekdays={weekdays} open={open} />}
+      open={open}
+      setOpen={setOpen}
+      ariaLabel="Weekday range picker"
+    >
+      <div className="flex flex-col gap-2">
+        <Calendar selectedDays={weekdays} onChange={setWeekdayRange} />
+        <WeekdayRangePresets
+          weekdays={weekdays}
+          setWeekdayRange={setWeekdayRange}
+        />
+      </div>
+    </DatePopover>
   );
 }
