4c95ca5640cd713ed822a9afa76a2ebd36b3291f
jzgom067 <jzgombic3@gmail.com>
2026-09-01 18:03:49 -0400

    Simplify weekday button

--- frontend/src/features/event/editor/dates/weekday-range/calendar.tsx
+++ frontend/src/features/event/editor/dates/weekday-range/calendar.tsx
@@ -29,25 +29,25 @@ export default function Calendar({
         const rightSelected = selectedIndices[index + 1];
 
         return (
-          <div key={day} onClick={() => handleDayClick(index)}>
-            <button
-              className={cn(
-                "flex h-8 w-10 items-center justify-center px-6",
-                "active:bg-accent/40 text-foreground/50 cursor-pointer",
+          <button
+            key={day}
+            onClick={() => handleDayClick(index)}
+            className={cn(
+              "flex h-8 w-10 items-center justify-center px-6",
+              "active:bg-accent/40 text-foreground/50 cursor-pointer",
 
-                // Highlighted State
-                isSelected
-                  ? "bg-accent/15 text-accent-text hover:bg-accent/30"
-                  : "hover:bg-accent/15 rounded-full",
+              // Highlighted State
+              isSelected
+                ? "bg-accent/15 text-accent-text hover:bg-accent/30"
+                : "hover:bg-accent/15 rounded-full",
 
-                // Contiguous Rounding Logic
-                !leftSelected && "rounded-l-full",
-                !rightSelected && "rounded-r-full",
-              )}
-            >
-              {day}
-            </button>
-          </div>
+              // Contiguous Rounding Logic
+              !leftSelected && "rounded-l-full",
+              !rightSelected && "rounded-r-full",
+            )}
+          >
+            {day}
+          </button>
         );
       })}
     </div>
