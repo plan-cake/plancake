8b9ad27ed0ee0fd397e3e2d5481bd61e9cd3e677
jzgom067 <jzgombic3@gmail.com>
2026-09-01 18:27:18 -0400

    Add drag to weekday calendar

--- frontend/src/features/event/editor/dates/weekday-range/calendar.tsx
+++ frontend/src/features/event/editor/dates/weekday-range/calendar.tsx
@@ -1,4 +1,5 @@
 import { ALL_WEEKDAYS, Weekday } from "@/core/event/types";
+import useWeekdayRangeDrag from "@/features/event/editor/dates/weekday-range/use-drag";
 import { cn } from "@/lib/utils/classname";
 
 export default function Calendar({
@@ -21,33 +22,87 @@ export default function Calendar({
     ALL_WEEKDAYS.map((day, index) => [index, selectedDays.has(day)]),
   );
 
+  const {
+    hoveredDay,
+    dragState,
+    handlePointerDown,
+    handlePointerEnter,
+    handlePointerLeave,
+    handleTouchMove,
+  } = useWeekdayRangeDrag({
+    selectedDays,
+    setDays: onChange,
+  });
+
   return (
     <div className="flex select-none flex-row flex-wrap">
       {ALL_WEEKDAYS.map((day, index) => {
+        const previousDay = ALL_WEEKDAYS[index - 1];
+        const nextDay = ALL_WEEKDAYS[index + 1];
         const isSelected = selectedIndices[index];
-        const leftSelected = selectedIndices[index - 1];
-        const rightSelected = selectedIndices[index + 1];
+        const isDragged = dragState.dragRange.has(day);
+        const isHovered = hoveredDay === day;
+
+        const groupStart =
+          (isSelected || isDragged) &&
+          !selectedIndices[index - 1] &&
+          !dragState.dragRange.has(previousDay);
+        const groupEnd =
+          (isSelected || isDragged) &&
+          !selectedIndices[index + 1] &&
+          !dragState.dragRange.has(nextDay);
+
+        const dragEnabling =
+          dragState.isDragging &&
+          dragState.isEnabling &&
+          isDragged &&
+          !isSelected;
+        const dragDisabling =
+          dragState.isDragging &&
+          !dragState.isEnabling &&
+          isDragged &&
+          isSelected;
 
         return (
-          <button
+          <div
             key={day}
-            onClick={() => handleDayClick(index)}
+            data-day={day}
             className={cn(
               "flex h-8 w-10 items-center justify-center px-6",
-              "active:bg-accent/40 text-foreground/50 cursor-pointer",
+              "text-foreground/50 cursor-pointer rounded-full",
 
-              // Highlighted State
-              isSelected
-                ? "bg-accent/15 text-accent-text hover:bg-accent/30"
-                : "hover:bg-accent/15 rounded-full",
+              // Selected State
+              isSelected &&
+                "bg-accent/15 text-accent-text rounded-none font-bold",
+
+              // Drag State
+              dragEnabling && "bg-accent/30 rounded-none",
+              dragDisabling && "bg-accent/40 rounded-none",
+
+              // Hovered State
+              isHovered &&
+                (isSelected ? "hover:bg-accent/40" : "hover:bg-accent/15"),
 
               // Contiguous Rounding Logic
-              !leftSelected && "rounded-l-full",
-              !rightSelected && "rounded-r-full",
+              groupStart && "rounded-l-full",
+              groupEnd && "rounded-r-full",
             )}
+            onPointerDown={() => handlePointerDown(day)}
+            onPointerEnter={() => handlePointerEnter(day)}
+            onPointerLeave={handlePointerLeave}
+            onTouchMove={handleTouchMove}
           >
-            {day}
-          </button>
+            <button
+              onClick={() => {
+                if (!dragState.isDragging) {
+                  handleDayClick(index);
+                }
+              }}
+              className="pointer-events-none"
+            >
+              {day}
+            </button>
+          </div>
         );
       })}
     </div>
