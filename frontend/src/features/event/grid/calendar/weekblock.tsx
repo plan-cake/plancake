2353f13170d1d510baa2d4a520a5ec7c6a0ee112
jzgom067 <jzgombic3@gmail.com>
2026-09-03 17:28:27 -0400

    Add background for nonexistent days

--- frontend/src/features/event/grid/calendar/weekblock.tsx
+++ frontend/src/features/event/grid/calendar/weekblock.tsx
@@ -9,6 +9,15 @@ export default function WeekBlock({ weeks }: { weeks: (string | null)[][] }) {
         "border-foreground/75 border",
         "divide-foreground/75 divide-y divide-dashed",
       )}
+      style={{
+        backgroundImage: `repeating-linear-gradient(
+          45deg, 
+          color-mix(in srgb, var(--color-foreground) 10%, transparent) 0px, 
+          color-mix(in srgb, var(--color-foreground) 10%, transparent) 8px, 
+          color-mix(in srgb, var(--color-background) 10%, transparent) 8px, 
+          color-mix(in srgb, var(--color-background) 10%, transparent) 9.5px
+        )`,
+      }}
     >
       {weeks.map((days, wIndex) => (
         <div
@@ -21,7 +30,10 @@ export default function WeekBlock({ weeks }: { weeks: (string | null)[][] }) {
             return (
               <div
                 key={dIndex}
-                className="h-20 flex-1 p-2 text-right leading-none"
+                className={cn(
+                  "h-20 flex-1 p-2 text-right leading-none",
+                  exists && "bg-panel",
+                )}
               >
                 {exists && parse(day, "yyyy-MM-dd", new Date()).getDate()}
               </div>
